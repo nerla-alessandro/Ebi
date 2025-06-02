91ccb000efd7570bb00829f1653ce00e7614e7a4
Lorenzo Tucci <lorenzo@unitoo.it>
2025-06-02 14:27:43 +0300

    fix(chore): switch from `tonic` to `prost`

--- file-manager/build.rs
+++ file-manager/build.rs
@@ -1,4 +1,4 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("src/rpc/ebi.proto")?;
+    prost_build::compile_protos(&["src/rpc/ebi.proto"], &["src/rpc"])?;
     Ok(())
 }
