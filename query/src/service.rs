89f973f0fe3dd8094424dbb9755d6a2c41229978
Alessandro Nerla <nerla.alessandro@gmail.com>
2026-01-28 22:38:53 +0100

    chore: replaced UUID v7 with UUID v4
    Replaced UUID v7 with UUID v4 where it was still used

--- query/src/service.rs
+++ query/src/service.rs
@@ -423,7 +423,7 @@ impl Service<ClientQuery> for QueryService {
                 1
             };
 
-            let token = Uuid::now_v7();
+            let token = Uuid::new_v4();
             let ser_token: Vec<u8> = Into::<Vec<u8>>::into(token);
 
             //[/] Return ClientQueryResponse in RpcService before asynchronously calling QueryService
@@ -619,7 +619,7 @@ impl Service<ClientQuery> for QueryService {
                             })
                             .collect(),
                         metadata: Some(Status {
-                            request_uuid: Into::<Vec<u8>>::into(Uuid::now_v7()),
+                            request_uuid: Into::<Vec<u8>>::into(Uuid::new_v4()),
                             return_code: ReturnCode::Success as u32, // [?] Should this always be success ??
                             error_data: Some(ErrorData { error_data: errors }),
                         }),
@@ -642,7 +642,7 @@ impl Service<ClientQuery> for QueryService {
                                             token: ser_token.clone(),
                                             files: Vec::new(),
                                             metadata: Some(Status {
-                                                request_uuid: Into::<Vec<u8>>::into(Uuid::now_v7()),
+                                                request_uuid: Into::<Vec<u8>>::into(Uuid::new_v4()),
                                                 return_code: ReturnCode::PeerServiceError as u32,
                                                 error_data: Some(ErrorData {
                                                     error_data: vec![format!(
@@ -682,7 +682,7 @@ impl Service<ClientQuery> for QueryService {
                                                 error_data: Some(ErrorData {
                                                     error_data: t_res.errors,
                                                 }),
-                                                request_uuid: Into::<Vec<u8>>::into(Uuid::now_v7()),
+                                                request_uuid: Into::<Vec<u8>>::into(Uuid::new_v4()),
                                             }),
                                         }),
                                     )
