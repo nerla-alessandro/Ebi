45b80fb002b66be4827dfcb216e78d80eda151a3
Alessandro Nerla <nerla.alessandro@gmail.com>
2026-01-28 22:39:19 +0100

    chore: `cargo fmt`

--- state/src/lib.rs
+++ state/src/lib.rs
@@ -95,9 +95,7 @@ impl StateChain {
     pub fn hash_synced(&self) -> Vec<u128> {
         self.synced
             .iter()
-            .map(|state_ref| {
-                state_ref.id.as_u128()
-            })
+            .map(|state_ref| state_ref.id.as_u128())
             .collect()
     }
 }
